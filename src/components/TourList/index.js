24d2606ec85a1ab50894946fb64afb92be4161de
vincepogz <85046947+vincepogz@users.noreply.github.com>
2022-01-20 23:43:06 -0500

    added pop up

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,12 +1,18 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import Tour from "../Tour";
 import "./tourlist.scss";
 import { tourData } from "../../tourData";
 
+ 
 export default class TourList extends Component {
+
+
+
     state = {
-        tours: tourData
+        tours: tourData,
+        display: false
     };
+
     removeTour = id => {
         const { tours } = this. state;
         const sortedTours = tours.filter(tour => tour.id !== id);
@@ -14,15 +20,43 @@ export default class TourList extends Component {
             tours: sortedTours
         });
     };
+
+    close = () => {
+        this.setState({
+            display: !this.state.display
+        });
+    };
+
     render() {
         // console.log(this.state.tours);
         const { tours } = this.state;
 
         return (
+
+            
             <section className="tourlist">
+               
+
+                <div className="popUp">
+                    <span class="close" >&times;</span>
+                    <div className="gallery">
+
+                        <div className="popUp-content">
+
+                            Hello
+
+                        </div>
+                    </div>
+
+
+                </div>
+
                 {tours.map(tour => (
                     <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
                 ))}
+
+
+
             </section>
         );
     }
