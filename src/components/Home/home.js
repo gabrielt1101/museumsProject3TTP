eb2071e67271e2c288a9a0ba5f8e2be60e000ab7
Asia Gibson <gibsonjasia@gmail.com>
2022-01-21 16:24:35 -0500

    added home page to site

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./home.css"
 
 const Home = () => {
     return(
-        <div>
-            <h1>Home</h1>
+        <div class="banner-are">
+        
+
+            <div className="banner_page">
+                <h1 className="head"> Fun Museums to See During Your Stay ! </h1>
+                <img src="img/nyc.jpg" alt="Girl in a jacket" className="img"></img>
+            </div>
+
         </div>
     );
 };
